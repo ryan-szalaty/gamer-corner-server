f3e975e54cddbcd9a0418263a8032a51994452da
ryan-szalaty <ryan.szalaty@gmail.com>
2022-08-24 08:32:50 +0900

    First commit.

---
+++ server/server.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("express");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
+const server = express();
+
+server.use(express.static(path.resolve(__dirname, "../client/build")));
+server.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.json());
+
+/*const db = mysql.createConnection({
+  host: process.env.DEV_DATABASE_HOST,
+  user: process.env.DEV_DATABASE_USER,
+  database: process.env.DEV_DATABASE,
+  password: process.env.DEV_DATABASE_PASS,
+});*/
+
+const db = mysql.createConnection({
+  host: process.env.PROD_DATABASE_HOST,
+  user: process.env.PROD_DATABASE_USER,
+  database: process.env.PROD_DATABASE,
+  password: process.env.PROD_DATABASE_PASS,
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  else {
+    console.log("Database connected!");
+  }
+});
+
+server.get("/create_database", (req, res) => {
+  let sql = "CREATE DATABASE gamerCorner";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    else {
+      console.log("Created database:", result);
+    }
+  });
+});
+
+server.get("/create_table", (req, res) => {
+  let sql =
+    "CREATE TABLE Users (UserId INT AUTO_INCREMENT, Username VARCHAR(255), Email VARCHAR(255), Password VARCHAR(255), PRIMARY KEY (UserId))";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    else {
+      console.log("Created table:", result);
+    }
+  });
+});
+
+server.get("/get_data", (req, res) => {
+  let sql = "SELECT * FROM Users";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    else {
+      res.send(result);
+    }
+  });
+});
+
+server.post("/register", (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  let sql = "INSERT INTO Users (username, email, password) VALUES (?, ?, ?)";
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
+    db.query(sql, [username, email, hashedPassword], (err, result) => {
+      if (err) throw err;
+      else {
+        console.log("Successfully registered. New user: ", {
+          name: username,
+          mail: email,
+          pass: hashedPassword,
+        });
+      }
+    });
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server listening on ${process.env.PORT}`);
+});
