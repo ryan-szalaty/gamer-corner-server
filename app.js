3ec735fd0051d83cbee91c8526c74536cfc75d0e
ryan-szalaty <ryan.szalaty@gmail.com>
2022-08-24 09:39:40 +0900

    First commit.

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("express");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+const app = express();
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.set("view engine", "ejs");
+
+app.use(
+    cors({
+      origin: true,
+      methods: ["GET", "POST"],
+      credentials: true,
+    })
+  );
+  
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on PORT ${port}!`);
+});
